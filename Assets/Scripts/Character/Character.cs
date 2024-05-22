e8e63b38a050895bf9632a43640d5a7ceccfdd39
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-05-22 17:11:23 -0400

    write character data

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacterProperties;
+
+
+public class Character  
+{
+    private HP m_hp;
+    private SAN m_san;
+    private CharacterProperties.Time m_time;
+
+
+    private Hunger m_hunger;
+    private Thirst m_thirst;
+    private Sleep m_sleep;
+    private Illness m_illness;
+    private Mood m_mood;
+
+    private Intelligent m_intelligent;
+    private Mind m_mind;
+    private Strength m_strength;
+    private Speed m_speed;
+
+
+}
