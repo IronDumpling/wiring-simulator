b178a317738f95e2156a3cc6fbc162649d8616d7
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-07-07 12:54:11 -0400

    add map selection state template

--- Assets/Scripts/Core/ActionStates/MapSelectionState.cs
+++ Assets/Scripts/Core/ActionStates/MapSelectionState.cs
@@ -2,6 +2,37 @@
 {
     public class MapSelectionState: ActionState
     {
-        
+        private int m_nodeIdx;
+        public MapSelectionState(int nodeIdx)
+        {
+            m_nodeIdx = nodeIdx;
+        }
+
+        public override void Enter(ActionState last)
+        {
+            // disable Other UI
+            // register call back function
+            
+            // Find all possible path
+            // Register Possible Path call back
+            
+            //() => OnPathPressed(pathIdx)
+        }
+
+        public override void Exit()
+        {
+            // clear all call back
+            // remove all listener
+        }
+
+        private void OnCurrentNodePressed()
+        {
+            // go back to Idle State
+        }
+
+        private void OnPathPressed(int pathIdx)
+        {
+            // enter Path state
+        }
     }
 }
